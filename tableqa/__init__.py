27ee9c0196b9498582ae9339cfde1156466cf30e
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2020-09-01 23:35:46 +0530

    Update tableqa/__init__.py

--- tableqa/__init__.py
+++ tableqa/__init__.py
@@ -4,7 +4,6 @@ from .nlp import *
 from .data_utils import *
 from .conditionmaps import *
 from .column_types import *
-from .clf import *
 from .clauses import *
 from .database import *
 from .agent import *
