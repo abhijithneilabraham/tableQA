ba2625360fc587ae2ea2844a884d485d64b8dbf6
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2020-09-02 21:47:35 +0530

    publishing a new model for clf, and removing dependency of sentence_transformers

--- tableqa/clauses.py
+++ tableqa/clauses.py
@@ -1,17 +1,27 @@
-
+import tensorflow as tf
 from tensorflow.keras.models import load_model
-from sentence_transformers import SentenceTransformer
+from transformers import BertTokenizer, TFBertModel
 from numpy import asarray
 import os
 
 class Clause:
     def __init__(self):
-        self.bert_model = SentenceTransformer('bert-base-nli-mean-tokens')
-        self.model=load_model(os.path.join(os.path.abspath(os.path.dirname(__file__)),"Question_Classifier.h5"))
+        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+        self.bert_model = TFBertModel.from_pretrained('bert-base-uncased')
+        self.model=load_model(os.path.join(os.path.abspath(os.path.dirname(__file__)),"Question_Classifier_Bert.h5"))
         self.types={0:'SELECT {} FROM {}', 1:'SELECT MAX({}) FROM {}', 2:'SELECT MIN({}) FROM {}', 3:'SELECT COUNT({}) FROM {}', 4:'SELECT SUM({}) FROM {}', 5:'SELECT AVG({}) FROM {}'}
+    
+    def get_bert_embeddings(self,x):
+        emb=[]
+        for i,sentence in enumerate(x):
+            input_ids = tf.constant(self.tokenizer.encode(sentence))[None, :]  # Batch size 1
+            outputs = self.bert_model(input_ids)
+            last_hidden_states = outputs[0][0][1] 
+            emb.append(last_hidden_states)
+        return asarray(emb)
 
     def adapt(self,q,inttype=False,summable=False):
-        emb=asarray(self.bert_model.encode(q))
+        emb=self.get_bert_embeddings([q])
         self.clause=self.types[self.model.predict_classes(emb)[0]]
         
         if summable and inttype  and "COUNT" in self.clause:
