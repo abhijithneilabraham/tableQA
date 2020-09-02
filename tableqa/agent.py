3e8a0ae8d9183349de42c1402212b7d39e9fadcf
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2020-09-02 09:35:49 +0530

    bug fix

--- tableqa/agent.py
+++ tableqa/agent.py
@@ -27,12 +27,13 @@ class Agent:
         nlp=Nlp(self.data_dir,self.schema_dir)
         csv = nlp.csv_select(question)
         if csv is None:
-            print("Sorry,didn't catch that")    
-        question, valmap = nlp.get_sql_query(csv, question)
-        sql_query=question
-        for k, v in valmap.items():
-            sql_query = sql_query.replace(k, v)
-        
-        return sql_query
- 
+            print("Sorry,didn't catch that")  
+        else:
+            question, valmap = nlp.get_sql_query(csv, question)
+            sql_query=question
+            for k, v in valmap.items():
+                sql_query = sql_query.replace(k, v)
+            
+            return sql_query
+     
 
