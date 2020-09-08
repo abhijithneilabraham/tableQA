2d946c14c2559627d54a3d91459a474974528a10
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2020-09-08 16:15:12 +0530

    fix for nltk downloads

--- tableqa/__init__.py
+++ tableqa/__init__.py
@@ -1,3 +1,14 @@
+def _nltk_downloader():
+    try:
+        nltk.download('wordnet',quiet=True)
+        nltk.download('averaged_perceptron_tagger',quiet=True)
+        nltk.download('stopwords',quiet=True)
+        nltk.download('punkt',quiet=True)
+    except LookupError as e:
+        print(e)
+
+_nltk_downloader()
+
 
 from .nlp import *
 from .data_utils import *
