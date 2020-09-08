e125a9d96cde6f8a605059e8e6925ef73f56aa60
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-09-09 02:28:21 +0530

    Tests for sql agent (#13)
    * update tests for Clause()
    
    * Update .travis.yml
    
    * pytest integration with travis CI
    
    * Update pytest.ini
    
    * bug fix
    
    * Update tableqa/tests/test_Clause.py
    
    * bug fix
    
    * bug fix
    
    * Update .travis.yml
    
    * updating requirements
    
    * Update .travis.yml
    
    * Update tableqa/agent.py
    
    * fix for nltk downloads
    
    * Update tableqa/__init__.py
    
    * Update .travis.yml
    
    * tests for sql agent
    
    * Update tests/test_Agent.py
    
    * Update .travis.yml

--- tableqa/clauses.py
+++ tableqa/clauses.py
@@ -4,15 +4,16 @@ from sentence_transformers import SentenceTransformer
 from numpy import asarray,argmax
 import os
 
+bert_model = SentenceTransformer('bert-base-nli-mean-tokens')
+model=load_model(os.path.join(os.path.abspath(os.path.dirname(__file__)),"Question_Classifier.h5"))
+
 class Clause:
     def __init__(self):
-        self.bert_model = SentenceTransformer('bert-base-nli-mean-tokens')
-        self.model=load_model(os.path.join(os.path.abspath(os.path.dirname(__file__)),"Question_Classifier.h5"))
         self.types={0:'SELECT {} FROM {}', 1:'SELECT MAX({}) FROM {}', 2:'SELECT MIN({}) FROM {}', 3:'SELECT COUNT({}) FROM {}', 4:'SELECT SUM({}) FROM {}', 5:'SELECT AVG({}) FROM {}'}
 
     def adapt(self,q,inttype=False,summable=False):
-        emb=asarray(self.bert_model.encode([q]))
-        self.clause=self.types[argmax(self.model.predict(emb),axis=-1)[0]]        
+        emb=asarray(bert_model.encode([q]))
+        self.clause=self.types[argmax(model.predict(emb),axis=-1)[0]]        
         if summable and inttype  and "COUNT" in self.clause:
             self.clause= '''SELECT SUM({}) FROM {}'''
         return self.clause
