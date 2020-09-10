9a67a9579dc6ac535ca3fd63692ff7ee6303a246
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-09-10 21:54:30 +0530

    code refactor for agent and database (#14)
    * update tests for Clause()
    
    * Update .travis.yml
    
    * pytest integration with travis CI
    
    * Update pytest.ini
    
    * bug fix
    
    * Update tableqa/tests/test_Clause.py
    
    * bug fix
    
    * bug fix
    
    * Update .travis.yml
    
    * updating requirements
    
    * Update .travis.yml
    
    * Update tableqa/agent.py
    
    * fix for nltk downloads
    
    * Update tableqa/__init__.py
    
    * Update .travis.yml
    
    * tests for sql agent
    
    * Update tests/test_Agent.py
    
    * Update .travis.yml
    
    * code refactor
    
    * code refactor
    
    * Update tests/test_agent.py
    
    * Delete test_Agent.py

--- tableqa/database.py
+++ tableqa/database.py
@@ -2,25 +2,16 @@
 
 from sqlalchemy import create_engine,types
 import ast
-from .agent import Agent
 from .column_types import get
 from .data_utils import data_utils
 from .nlp import Nlp
 
 
 class Database:
-    def __init__(self,data_dir,*args,**kwargs):
-        if args:
-            self.schema_dir=args[0]
-        else:
-            self.schema_dir=None
-        self.data_dir=data_dir
-        self.data_process=data_utils(data_dir, self.schema_dir)
-        self.nlp=Nlp(data_dir,self.schema_dir)
-    def Query_Sqlite(self,question):
-        agent=Agent(self.data_dir,self.schema_dir)
-        query=agent.get_response(question)   
-        print("sql query: {}".format(query))
+    def __init__(self,data_dir,schema_dir):
+        self.data_process=data_utils(data_dir,schema_dir)
+        self.nlp=Nlp(data_dir,schema_dir)
+    def sqlite(self,question):
         engine = create_engine('sqlite://', echo=False)
         csv=self.nlp.csv_select(question)
         data_frame=self.data_process.get_dataframe(csv).astype(str)
@@ -39,8 +30,7 @@ class Database:
                 coltype = getattr(types, coltype)()
             sql_schema[colname] = coltype
         data_frame.to_sql(schema['name'].lower(), con=engine, if_exists='replace', dtype=sql_schema)
-
-        return engine.execute(query).fetchall()
+        return engine
 
 
 
