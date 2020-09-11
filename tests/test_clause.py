0eb4c6259a452670b5ae1778b311aa842f00c480
Abhijith Neil Abraham <abhijithneilabrahampk@gmail.com>
2020-09-11 21:48:43 +0530

    fixes and readme updates (#15)
    * update tests for Clause()
    
    * Update .travis.yml
    
    * pytest integration with travis CI
    
    * Update pytest.ini
    
    * bug fix
    
    * Update tableqa/tests/test_Clause.py
    
    * bug fix
    
    * bug fix
    
    * Update .travis.yml
    
    * updating requirements
    
    * Update .travis.yml
    
    * Update tableqa/agent.py
    
    * fix for nltk downloads
    
    * Update tableqa/__init__.py
    
    * Update .travis.yml
    
    * tests for sql agent
    
    * Update tests/test_Agent.py
    
    * Update .travis.yml
    
    * code refactor
    
    * code refactor
    
    * Update tests/test_agent.py
    
    * Delete test_Agent.py
    
    * fix for tests
    
    * Update README.md
    
    * Update README.md

--- tests/test_clause.py
+++ tests/test_clause.py
@@ -4,8 +4,8 @@ import pytest
 def test_clause():
     clause=Clause()
     types={"SELECT {} FROM {}":["which are the activities in 2011","find who died of stomach cancer"],
-           "SELECT MAX({}) FROM {}":["find the highest gdp value","when was the most number of cases reported","what was the maximum age of stomach cancer patients"],
-           "SELECT MIN({}) FROM {}":["find the lowest gdp value","when was the least number of cases reported","what was the minimum age of stomach cancer patients"],
+           "SELECT MAX({}) FROM {}":["find the highest gdp value","what was the most number of cases reported","what was the maximum age of stomach cancer patients"],
+           "SELECT MIN({}) FROM {}":["find the lowest gdp value","what was the least number of cases reported","what was the minimum age of stomach cancer patients"],
            "SELECT COUNT({}) FROM {}":["how many men died of stomach cancer in 2011","Find the number of activities in this data","Amount of people who died of cancer"],
            "SELECT SUM({}) FROM {}":["sum of all cases","sum of death count"],
            "SELECT AVG({}) FROM {}":["what is the average gdp last year","find the average number of reported cases","get me the average number of reported cases"]}
