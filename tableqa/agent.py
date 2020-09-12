d607e2a1d59a6737aae4115d82500ed008015d78
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2020-09-12 12:34:27 +0530

    Updating docstrings

--- tableqa/agent.py
+++ tableqa/agent.py
@@ -1,20 +1,35 @@
 
 
-import os
-import nltk
-from sqlalchemy import create_engine,types
-import ast
-from .column_types import get
-from .data_utils import data_utils
 from .nlp import Nlp
 from .database import Database
 
 class Agent:
+    """
+    Generates sql queries and fetches query results from database.
+    """
     def __init__(self,data_dir,schema_dir=None,db_type='sqlite'):
+        """
+        # Arguments
+        
+        data_dir: `str` or `pathlib.Path` object, absolute path to folder containing all input files.
+        schema_dir: `str` or `pathlib.Path` object, path to folder containing `json` schemas of input files. 
+                    If not specified, auto-generated schema will be used.
+        db_type: `str`, defining the type of Database object to be used.
+                If not specified, default object is `sqlite`.
+        """
         self.data_dir=data_dir
         self.schema_dir=schema_dir
         self.db_type=db_type
     def get_query(self,question):
+        """
+        # Arguments
+        
+        question: `str`,containing input utterance.
+        
+        # Returns
+        
+        Returns a  `str` of generated sql query.
+        """
         nlp=Nlp(self.data_dir,self.schema_dir)
         csv = nlp.csv_select(question)
         if csv is None:
@@ -28,6 +43,15 @@ class Agent:
             return sql_query
         
     def query_db(self,question):
+        """
+        # Arguments
+        
+        question: `str`,containing input utterance.
+        
+        # Returns
+        
+        Returns a  `list` of 'tuple` of query outputs from Database.
+        """
         query=self.get_query(question)
         database=Database(self.data_dir,self.schema_dir) 
         create_db=getattr(database, self.db_type)
