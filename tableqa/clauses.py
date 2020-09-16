6626188adbda8931533b7b6d45a2425751c1ca0f
nandanasreeraj123 <nandanasreeraj@gmail.com>
2020-09-16 12:25:21 -0700

    clf.py and clauses.py

--- tableqa/clauses.py
+++ tableqa/clauses.py
@@ -1,23 +1,32 @@
 
 from tensorflow.keras.models import load_model
-from sentence_transformers import SentenceTransformer
+import tensorflow_hub as hub
 from numpy import asarray,argmax
 import os
 
-bert_model = SentenceTransformer('bert-base-nli-mean-tokens')
+embed = hub.load('https://tfhub.dev/google/universal-sentence-encoder/4')
 model=load_model(os.path.join(os.path.abspath(os.path.dirname(__file__)),"Question_Classifier.h5"))
 
-class Clause:
-    def __init__(self):
-        self.types={0:'SELECT {} FROM {}', 1:'SELECT MAX({}) FROM {}', 2:'SELECT MIN({}) FROM {}', 3:'SELECT COUNT({}) FROM {}', 4:'SELECT SUM({}) FROM {}', 5:'SELECT AVG({}) FROM {}'}
 
-    def adapt(self,q,inttype=False,summable=False):
-        emb=asarray(bert_model.encode([q]))
-        self.clause=self.types[argmax(model.predict(emb),axis=-1)[0]]        
-        if summable and inttype  and "COUNT" in self.clause:
-            self.clause= '''SELECT SUM({}) FROM {}'''
+class Clause:
+
+
+    def __init__(self):
+        self.types = {0: 'SELECT {} FROM {}', 1: 'SELECT MAX({}) FROM {}', 2: 'SELECT MIN({}) FROM {}',
+                      3: 'SELECT COUNT({}) FROM {}', 4: 'SELECT SUM({}) FROM {}', 5: 'SELECT AVG({}) FROM {}'}
+
+    def get_universal_sentence_encoder(self, x):
+        embeddings = embed(x)
+        return asarray(embeddings)
+
+    def adapt(self, q, inttype=False, summable=False):
+        emb = self.get_universal_sentence_encoder([q])
+        self.clause = self.types[argmax(model.predict(emb))]
+
+        if summable and inttype and "COUNT" in self.clause:
+            self.clause = '''SELECT SUM({}) FROM {}'''
         return self.clause
-    
+
     
 
 
