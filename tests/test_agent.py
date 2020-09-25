84735be443f6e017f4900b87a76856b485ff6590
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2020-09-26 00:10:49 +0530

    modified tests

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -10,7 +10,7 @@ def test_query():
     agent=Agent(os.path.join(currpath,"cleaned_data"))
     qmaps={'how many nuclear medicine activities in 2012':'SELECT COUNT(activity_type) FROM activities_data WHERE activity_type_chapter = "Nuclear Medicine" AND year = "2012" ',
             'find me the diseases having above 3000 cases':'SELECT disease FROM communicable_diseases_data WHERE cases  > 3000 ',
-            'which are the activities in 2011':'SELECT activity_type FROM activities_data WHERE year = "2011" ',
+            'which are the activities in 2011':'SELECT activity_type,activity_type_chapter FROM activities_data WHERE year = "2011" ',
             'find the maximum number of cases':'SELECT MAX(cases) FROM communicable_diseases_data',
             'Get me the average age of stomach cancer deaths':'SELECT AVG(death_count) FROM cancer_death_data WHERE cancer_site = "Stomach" '
             }
@@ -23,7 +23,7 @@ def test_query_schema():
     agent=Agent(os.path.join(currpath,"cleaned_data"),os.path.join(currpath,"schema"))
     qmaps={'how many people died of stomach cancer in 2011':'SELECT SUM(death_count) FROM cancer_death WHERE cancer_site = "Stomach" AND year = "2011" ',
             'how many deaths of age below 40 had stomach cancer':'SELECT SUM(death_count) FROM cancer_death WHERE cancer_site = "Stomach" AND age  < 40 ',
-            'which are the activities in 2011':'SELECT activity_type_chapter FROM activities WHERE year = "2011" ',
+            'which are the activities in 2011':'SELECT activity_type_chapter,activity_type FROM activities WHERE year = "2011" ',
             'find the maximum number of cases':'SELECT MAX(cases) FROM communicable_diseases ',
             'Get me the average age of stomach cancer deaths':'SELECT AVG(death_count) FROM cancer_death WHERE cancer_site = "Stomach" '
             }
