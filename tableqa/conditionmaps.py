0d1d5d7bb99696187e952fdf111cc9227e050a28
abhijithneilabraham <abhijithneilabrahampk@gmail.com>
2020-09-28 20:04:18 +0530

    conditional ops improvement

--- tableqa/conditionmaps.py
+++ tableqa/conditionmaps.py
@@ -2,7 +2,7 @@
 
 
 conditions={
-  "<":["below","low","less","small"],
-  ">":["above","high","great","big"]
+  "<":["below","low","less","small","lesser","smaller","lower"],
+  ">":["above","high","great","more","big","bigger","greater","larger","higher"]
 }
 
